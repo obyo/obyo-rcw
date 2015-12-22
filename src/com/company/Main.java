f5bac2015db2e12aa6bd0dceef3ccb4aee3ca795
yoshiki obata <obatay@c240161.intranet.justsystem.co.jp>
2015-12-22 11:47:41 +0900

    spec1:  "オムライス"の出力処理追加

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        showRecipeTitle("オムライス");
+    }
+
+    /**
+     * レシピのタイトルを出力する
+     * タイトルが不正なら何もしない
+     *
+     * @param title 出力したいタイトル
+     */
+    private static void showRecipeTitle(String title) {
+        if (Util.isEmpty(title)) {
+            return;
+        }
+        System.out.println(title);
     }
 }
